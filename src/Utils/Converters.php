dae979b1958b21cf0b8385b41f1790f7b821583e
Mamut010 <phucthinh0013@gmail.com>
2024-11-08 22:32:53 +0800

    Update: Converters can auto initialize nullable fields

--- src/Utils/Converters.php
+++ src/Utils/Converters.php
@@ -23,28 +23,34 @@ class Converters
     }
 
     public static function arrayToObject(array $array, string|object $objOrClass, mixed ...$ctorArgs): object|false {
-        $obj = is_string($objOrClass) ? Reflections::instantiateClass($objOrClass, ...$ctorArgs) : $objOrClass;
-        if (!$obj) {
-            return false;
-        }
-        
-        foreach ($array as $key => $value) {
-            if (property_exists($obj, $key)) {
-                $obj->{$key} = $value;
-            }
-        }
-        return $obj;
+        $valueChecker = fn(string $propName) => array_key_exists($propName, $array);
+        $valueGetter = fn(string $propName) => $array[$propName];
+        return static::createAndInitObjectValues($valueChecker, $valueGetter, $objOrClass, ...$ctorArgs);
     }
 
     public static function instanceToObject(object $instance, string|object $objOrClass, mixed ...$ctorArgs): object|false {
+        $valueChecker = fn(string $propName) => property_exists($instance, $propName);
+        $valueGetter = fn(string $propName) => $instance->{$propName};
+        return static::createAndInitObjectValues($valueChecker, $valueGetter, $objOrClass, ...$ctorArgs);
+    }
+
+    private static function createAndInitObjectValues(
+        callable $valueChecker, callable $valueGetter,
+        string|object $objOrClass, mixed ...$ctorArgs): object|false {
         $obj = is_string($objOrClass) ? Reflections::instantiateClass($objOrClass, ...$ctorArgs) : $objOrClass;
         if (!$obj) {
             return false;
         }
 
-        foreach ($instance as $key => $value) {
-            if (property_exists($obj, $key)) {
-                $obj->{$key} = $value;
+        $reflector = new \ReflectionObject($obj);
+        $props = $reflector->getProperties(\ReflectionProperty::IS_PUBLIC);
+        foreach ($props as $prop) {
+            $propName = $prop->getName();
+            if (call_user_func($valueChecker, $propName)) {
+                $obj->{$propName} = call_user_func($valueGetter, $propName);
+            }
+            elseif (!$prop->isInitialized($obj) && $prop->getType()?->allowsNull()) {
+                $obj->{$propName} = null;
             }
         }
         return $obj;
